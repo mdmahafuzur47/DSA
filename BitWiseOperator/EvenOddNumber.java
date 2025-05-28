0626f7a007d055db13980064db7ab392dc930af1
mdmahafuzur47 <mdmahafuzur4747@gmail.com>
2025-05-28 22:15:59 +0600

    feat: create EvenOddNumber.java

---
+++ BitWiseOperator/EvenOddNumber.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class EvenOddNumber {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = scanner.nextInt();
+
+        System.out.print(n);
+
+    }
+}
