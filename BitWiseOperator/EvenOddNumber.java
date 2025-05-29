1c080be9de37b3bfc98cf07dee35bf3031f8b4a9
mdmahafuzur47 <mdmahafuzur4747@gmail.com>
2025-05-29 20:45:11 +0600

    update EvenOddNumber.java

--- BitWiseOperator/EvenOddNumber.java
+++ BitWiseOperator/EvenOddNumber.java
@@ -6,7 +6,11 @@ public class EvenOddNumber {
         System.out.print("Enter the number: ");
         int n = scanner.nextInt();
 
-        System.out.println(n);
+       if((n & 1) == 0){
+           System.out.println(n+" is Even number");
+       }else{
+           System.out.println(n+" is Odd number");
+       }
 
     }
 }
